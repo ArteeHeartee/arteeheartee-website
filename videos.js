651adedba971b08b62ca3dd0e73835b1d0db3dfc
ArteeHeartee <andreasrtee@gmail.com>
2026-06-24 23:52:54 +0200

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,1 +1,40 @@
-const videos = [];
+const featuredVideo = {
+    title: "Waking up with pets",
+    description: "The best way to start the day.",
+    youtube: "https://www.youtube.com/shorts/Kfq2Qg3gm30",
+    tiktok: "https://www.tiktok.com/@arteeheartee/video/7607923276530109718",
+    youtubeId: "Kfq2Qg3gm30"
+};
+
+const topVideos = [
+    {
+        title: "Working From Home With Two Cats",
+        youtube: "https://www.youtube.com/shorts/gnfrCU0igsI",
+        tiktok: "https://www.tiktok.com/@arteeheartee/video/7606044634170641686",
+        youtubeId: "gnfrCU0igsI"
+    },
+    {
+        title: "Wearing Black Jeans as a Ragdoll Owner",
+        youtube: "https://www.youtube.com/shorts/jNUHEdZkYVk",
+        tiktok: "https://www.tiktok.com/@arteeheartee/video/7604183387460390166",
+        youtubeId: "jNUHEdZkYVk"
+    },
+    {
+        title: "Different Ways to Hold Your Cat",
+        youtube: "https://www.youtube.com/shorts/LZ2gcbexIZw",
+        tiktok: "https://www.tiktok.com/@arteeheartee/video/7637570031886290198",
+        youtubeId: "LZ2gcbexIZw"
+    },
+    {
+        title: "The Ragdoll Test",
+        youtube: "https://www.youtube.com/shorts/TAIG_NdAYr8",
+        tiktok: "https://www.tiktok.com/@arteeheartee/video/7622007836112850198",
+        youtubeId: "TAIG_NdAYr8"
+    },
+    {
+        title: "How I Used to Dress Before Getting Two Cats",
+        youtube: "https://www.youtube.com/shorts/VB9ZnB8G7nA",
+        tiktok: "https://www.tiktok.com/@arteeheartee/video/7611978752582585602",
+        youtubeId: "VB9ZnB8G7nA"
+    }
+];
